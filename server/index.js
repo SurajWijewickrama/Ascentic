fcfa3a52638cee20166497e7ec502798d058fe50
ManupaDev <manupadevsuite@gmail.com>
2023-10-02 00:14:00 +0530

    app cors

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
-app.use(cors());
+app.use(cors("*"));
 const http = require("http");
 
 const { Server } = require("socket.io");
