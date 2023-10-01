ce7876a33a9fb398144938cf00a4d3fdf2aee450
ManupaDev <manupadevsuite@gmail.com>
2023-10-01 23:16:58 +0530

    feat: server added

---
+++ server/entities/player.js
@@ -0,0 +1,8 @@
+class Player {
+  constructor(name, position, rotation, color) {
+    this.name = name;
+    this.rotation = rotation;
+    this.position = position;
+    this.color = color;
+  }
+}
---
+++ server/index.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+app.use(cors());
+const http = require("http");
+
+const { Server } = require("socket.io");
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
+});
+
+const players = {};
+const speed = 0.5;
+io.on("connection", (socket) => {
+  console.log("A user connected", socket.id);
+  players[socket.id] = {
+    name: "Unknown",
+    position: [10 * Math.random(), 1.5, 10 * Math.random()],
+    rotation: [0, Math.random(), 0],
+    color: "#ffffff",
+  };
+
+  console.log(players);
+
+  socket.on("connect", (data) => {
+    io.emit("connect_user", data);
+  });
+
+  socket.on("keyDown", (key) => {
+    const car = players[socket.id];
+    switch (key) {
+      case "KeyW":
+        car.position[2] += Math.cos(car.rotation[1]) * speed;
+        car.position[0] += Math.sin(car.rotation[1]) * speed;
+        break;
+      case "KeyS":
+        car.position[2] -= Math.cos(car.rotation[1]) * speed;
+        car.position[0] -= Math.sin(car.rotation[1]) * speed;
+        break;
+      case "KeyA":
+        car.rotation[1] += speed * 0.05;
+        break;
+      case "KeyD":
+        car.rotation[1] -= speed * 0.05;
+    }
+  });
+
+  socket.on("colorUpdated", (color) => {
+    players[socket.id].color = color;
+  });
+
+  socket.on("disconnect", (reason) => {
+    console.log(reason);
+    delete players[socket.id];
+  });
+});
+
+setInterval(() => {
+  io.emit("players", players);
+}, 15);
+
+server.listen(3001, () => {
+  console.log("Server is running on port 3001");
+});
