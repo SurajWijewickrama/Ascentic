69eddd867e2c8fce75e505e5fef090d9d06fb764
ManupaDev <manupadevsuite@gmail.com>
2023-10-01 23:59:20 +0530

    any cors

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "https://hoverspace.vercel.app/"],
+    origin: "*",
     methods: ["GET", "POST"],
   },
 });
