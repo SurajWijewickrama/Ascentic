60c00479d844fc0a28663203a61bf5a4d9134e29
ManupaDev <manupadevsuite@gmail.com>
2023-10-02 00:09:18 +0530

    try catch

--- server/index.js
+++ server/index.js
@@ -18,53 +18,58 @@ const io = new Server(server, {
 
 const players = {};
 const speed = 0.5;
-io.on("connection", (socket) => {
-  console.log("A user connected", socket.id);
-  players[socket.id] = {
-    name: "Unknown",
-    position: [10 * Math.random(), 1.5, 10 * Math.random()],
-    rotation: [0, Math.random(), 0],
-    color: "#ffffff",
-  };
 
-  console.log(players);
+try {
+  io.on("connection", (socket) => {
+    console.log("A user connected", socket.id);
+    players[socket.id] = {
+      name: "Unknown",
+      position: [10 * Math.random(), 1.5, 10 * Math.random()],
+      rotation: [0, Math.random(), 0],
+      color: "#ffffff",
+    };
 
-  socket.on("connect", (data) => {
-    io.emit("connect_user", data);
+    console.log(players);
+
+    socket.on("connect", (data) => {
+      io.emit("connect_user", data);
+    });
+
+    socket.on("keyDown", (key) => {
+      const car = players[socket.id];
+      switch (key) {
+        case "KeyW":
+          car.position[2] += Math.cos(car.rotation[1]) * speed;
+          car.position[0] += Math.sin(car.rotation[1]) * speed;
+          break;
+        case "KeyS":
+          car.position[2] -= Math.cos(car.rotation[1]) * speed;
+          car.position[0] -= Math.sin(car.rotation[1]) * speed;
+          break;
+        case "KeyA":
+          car.rotation[1] += speed * 0.05;
+          break;
+        case "KeyD":
+          car.rotation[1] -= speed * 0.05;
+      }
+    });
+
+    socket.on("colorUpdated", (color) => {
+      players[socket.id].color = color;
+    });
+
+    socket.on("disconnect", (reason) => {
+      console.log(reason);
+      delete players[socket.id];
+    });
   });
 
-  socket.on("keyDown", (key) => {
-    const car = players[socket.id];
-    switch (key) {
-      case "KeyW":
-        car.position[2] += Math.cos(car.rotation[1]) * speed;
-        car.position[0] += Math.sin(car.rotation[1]) * speed;
-        break;
-      case "KeyS":
-        car.position[2] -= Math.cos(car.rotation[1]) * speed;
-        car.position[0] -= Math.sin(car.rotation[1]) * speed;
-        break;
-      case "KeyA":
-        car.rotation[1] += speed * 0.05;
-        break;
-      case "KeyD":
-        car.rotation[1] -= speed * 0.05;
-    }
-  });
-
-  socket.on("colorUpdated", (color) => {
-    players[socket.id].color = color;
-  });
-
-  socket.on("disconnect", (reason) => {
-    console.log(reason);
-    delete players[socket.id];
-  });
-});
-
-setInterval(() => {
-  io.emit("players", players);
-}, 15);
+  setInterval(() => {
+    io.emit("players", players);
+  }, 15);
+} catch (error) {
+  console.log(error);
+}
 
 server.listen(3000, () => {
   console.log("Server is running on port 3001");
