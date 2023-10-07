a344c15bd8731cc5ad8e26ae5139ce4aace151dd
SurajWijewickrama <wijewickramasuraj@gmail.com>
2023-10-07 14:03:11 +0530

    sockets

--- 3fiberTrail/src/sockets/sockets.js
+++ 3fiberTrail/src/sockets/sockets.js
@@ -1,5 +1,10 @@
 import * as io from "socket.io-client";
 
-const socket = io.connect("https://hoverspace-production.up.railway.app/");
+const socket = io("https://hoverspace-server-production.up.railway.app/", {
+  withCredentials: true,
+  extraHeaders: {
+    "my-custom-header": "abcd"
+  }
+});
 
 export default socket;
